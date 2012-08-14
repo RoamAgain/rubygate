621be6fcd4ed78b0d69adda8c3e96db637bacb31
Akash Chauhan <akash6190@gmail.com>
2012-08-14 20:26:42 +0530

    reinitialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubygate::Application.config.secret_token = '2878d998b8477a8e96904eb661ce1d5b175b632d47270c07acd45f8f964122698986c83e241dc43e3218f8c05a0f86b19b062ecfafb3ddb09419667b4a6dd051'
